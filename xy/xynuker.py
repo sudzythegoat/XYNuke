45443ae0c0736e0903252d02b71ffee0ab21561d
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-15 11:50:53 -0600

    better response handling

--- xy/xynuker.py
+++ xy/xynuker.py
@@ -8,16 +8,16 @@ def read_webhooks():
         with open("webhooks.txt", "r") as webhookfile:
             return webhookfile.readlines()
     except FileNotFoundError:
-        print(Fore.RED + "Error: 'webhooks.txt' file not found.")
+        print(Fore.RED + "[>] Error: 'webhooks.txt' file not found.")
         return []
 
 def nuke(webhook, message):
     nukedata = {'content': message}
     response = requests.post(webhook, json=nukedata)
     if response.status_code == 200:
-        print(Fore.GREEN + f"Message sent to {webhook}")
+        print(Fore.GREEN + f"[>] Message sent to {webhook}")
     else:
-        print(Fore.RED + f"Failed to send message to {webhook}, Status Code: {response.status_code}")
+        print(Fore.RED + f"[>] Failed to send message to {webhook}")
 
 def main():
     webs = read_webhooks()
@@ -51,18 +51,18 @@ def main():
         options = input("[>] ")
         if options == "1":
             if not message:
-                print(Fore.YELLOW + "Please set a message")
+                print(Fore.YELLOW + "[>] Please set a message")
             else:
                 for webhook in webs:
                     nuke(webhook.strip(), message)
         elif options == "2":
-            message = input("Set nuke message\n[>] ")
+            message = input("[>] Set nuke message\n[>] ")
         elif options == "3":
             os.system("cls" if os.name == 'nt' else 'clear')
             time.sleep(0.5)
             webs = read_webhooks()
             webcount = len(webs)
         else:
-            print(Fore.RED + "Invalid")
+            print(Fore.RED + "[>] Invalid")
 if __name__ == "__main__":
     main()
